01b31856258215529886676453c5e41a8a35d2bd
syamim yusof <syamim_nsl@yahoo.com>
2023-05-31 00:30:45 +0800

    create signature using header

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const AWS = require("aws-sdk");
 const moment = require('moment');
 
 // parameter configuration
-const { 
-    SERVICE_NAME, 
-    REGION, 
-    SECRET_KEY, 
-    ACCESS_KEY, 
-    METHOD, 
-    API_PATH, 
-    END_POINT, 
-    EXPIRED 
+const {
+    SERVICE_NAME,
+    REGION,
+    SECRET_KEY,
+    ACCESS_KEY,
+    METHOD,
+    API_PATH,
+    END_POINT,
+    EXPIRED
 } = require('./config.js');
 
 
@@ -37,37 +37,80 @@ exports.CreateSig4 = async (event, callback) => {
         canonicalQuerystring += `&X-Amz-Date=${amzdate}`;
         canonicalQuerystring += `&X-Amz-Expires=${expired}`;
         canonicalQuerystring += `&X-Amz-SignedHeaders=host`;
-    
+
         var canonicalHeaders = `host:${host}\n`;
         var payloadHash = 'UNSIGNED-PAYLOAD';
         var canonicalRequest = `${method}\n${canonicalUri}\n${canonicalQuerystring}\n${canonicalHeaders}\nhost\n${payloadHash}`;
 
-        var stringToSign = algorithm + '\n' +  amzdate + '\n' +  credentialScope + '\n' +  SigV4Utils_sha256(canonicalRequest);
+        var stringToSign = algorithm + '\n' + amzdate + '\n' + credentialScope + '\n' + SigV4Utils_sha256(canonicalRequest);
         var signingKey = SigV4Utils_getSignatureKey(secretKey, dateStamp, region, service);
         var signature = SigV4Utils_sign(signingKey, stringToSign);
 
+        console.log('----------')
         canonicalQuerystring += '&X-Amz-Signature=' + signature;
         console.log('canonicalQuerystring : ', canonicalQuerystring)
+        console.log('----------')
+
+        // ---------------------------------------------------------------------------
+
+        var credentialScope = `${dateStamp}/${region}/${service}/aws4_request`;
+        var canonicalQuerystring = "";
+        var payloadHash = 'UNSIGNED-PAYLOAD';
+        var canonicalHeaders = [
+            `host:${host}`,
+            `x-amz-content-sha256:${payloadHash}`,
+            `x-amz-date:${amzdate}`,
+            `x-amz-expires:${expired}`,
+        ].join('\n');
+        var signedHeaders = canonicalHeaders.split('\n').map(header => header.split(':')[0]).join(';');
+
+        var canonicalRequest = [
+            `${method}`,
+            `${canonicalUri}`,
+            `${canonicalQuerystring}`,
+            `${canonicalHeaders}`,
+            '',
+            `${signedHeaders}`,
+            `${payloadHash}`,
+        ].join('\n')
+
+        var stringToSign = algorithm + '\n' + amzdate + '\n' + credentialScope + '\n' + SigV4Utils_sha256(canonicalRequest);
+        var signingKey = SigV4Utils_getSignatureKey(secretKey, dateStamp, region, service);
+        var signature = SigV4Utils_sign(signingKey, stringToSign);
+
+        console.log(canonicalHeaders)
+        console.log('----------')
+        console.log(canonicalRequest)
+        console.log('----------')
+        console.log(stringToSign)
+        console.log('----------')
+        console.log(signature)
+
+
+        const AuthHeader = `${algorithm} Credential=${accessKey}/${credentialScope},SignedHeaders=${signedHeaders},Signature=${signature}`
+        console.log('----------')
+        console.log('amzdate: ', amzdate)
+        console.log('AuthHeader : ', AuthHeader)
 
         callback(null, "Create signature success")
     }
     catch (err) {
-       callbackerror(err.statusCode, err.message, callback)
+        console.log(err)
     }
- }
+}
 
-function SigV4Utils_sign (key, msg) {
+function SigV4Utils_sign(key, msg) {
     return AWS.util.crypto.hmac(key, msg, 'hex');
 }
 
-function SigV4Utils_getSignatureKey (key, dateStamp, regionName, serviceName) {
+function SigV4Utils_getSignatureKey(key, dateStamp, regionName, serviceName) {
     var kDate = AWS.util.crypto.hmac('AWS4' + key, dateStamp, 'buffer');
     var kRegion = AWS.util.crypto.hmac(kDate, regionName, 'buffer');
     var kService = AWS.util.crypto.hmac(kRegion, serviceName, 'buffer');
-    var kCredentials = AWS.util.crypto.hmac(kService, 'aws4_request', 'buffer');    
+    var kCredentials = AWS.util.crypto.hmac(kService, 'aws4_request', 'buffer');
     return kCredentials;
 };
 
-function SigV4Utils_sha256 (msg) {
+function SigV4Utils_sha256(msg) {
     return AWS.util.crypto.sha256(msg, 'hex');
 };
